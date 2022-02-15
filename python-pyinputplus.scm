35b6b8c411fa17bd6f263df114503b59b2be7cbf
pyrenaica <strypsteen@posteo.net>
2022-02-15 22:01:55 +0100

    added an up-to-date version of yt-dlp

--- python-pyinputplus.scm
+++ python-pyinputplus.scm
@@ -1,5 +1,10 @@
 (define-module (python-pyinputplus)
   #:use-module (gnu packages python)
+  #:use-module (gnu packages video)
+  #:use-module (gnu packages music)
+  #:use-module (gnu packages python-crypto)
+  #:use-module (gnu packages python-web)
+  #:use-module (gnu packages check)
   #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages python-build)
   #:use-module (gnu packages python-check)
@@ -90,3 +95,73 @@ suitable for use in other Python 2 and 3 applications.")
    (synopsis "Software to identify the different types of hashes")
    (description "Software to identify the different types of hashes")
    (license license:gpl3+)))
+
+(define-public ls-yt-dlp
+  (package/inherit youtube-dl
+		   (name "yt-dlp")
+		   (version "2022.02.04")
+		   (source (origin
+			    (method url-fetch)
+			    (uri (string-append "https://github.com/yt-dlp/yt-dlp/"
+						"releases/download/"
+						version "/yt-dlp.tar.gz"))
+			    (sha256
+			     (base32
+			      "1qx8sx47lzyrcl00r2657zjaq0mwfbzjyfnv5lr5dlm552f13pf8"))
+			    (snippet
+			     '(begin
+				;; Delete the pre-generated files, except for the man page
+				;; which requires 'pandoc' to build.
+				(for-each delete-file '("yt-dlp"
+							;;pandoc is needed to generate
+							;;"yt-dlp.1"
+							"completions/bash/yt-dlp"
+							"completions/fish/yt-dlp.fish"
+							"completions/zsh/_yt-dlp"))
+				#t))))
+		   (arguments
+		    (substitute-keyword-arguments (package-arguments youtube-dl)
+						  ((#:tests? _) #t)
+						  ((#:phases phases)
+						   `(modify-phases ,phases
+								   ;; See the comment for the corresponding phase in youtube-dl.
+								   (replace 'default-to-the-ffmpeg-input
+									    (lambda _
+									      (substitute* "yt_dlp/postprocessor/ffmpeg.py"
+											   (("\\.get_param\\('ffmpeg_location'\\)" match)
+											    (format #f "~a or '~a'" match (which "ffmpeg"))))
+									      #t))
+								   (replace 'build-generated-files
+									    (lambda _
+									      ;; Avoid the yt-dlp.1 target, which requires pandoc.
+									      (invoke "make" "PYTHON=python" "yt-dlp" "completions")))
+								   (replace 'fix-the-data-directories
+									    (lambda* (#:key outputs #:allow-other-keys)
+									      (let ((prefix (assoc-ref outputs "out")))
+										(substitute* "setup.py"
+											     (("'etc/")
+											      (string-append "'" prefix "/etc/"))
+											     (("'share/")
+											      (string-append "'" prefix "/share/"))))
+									      #t))
+								   (delete 'install-completion)
+								   (replace 'check
+									    (lambda* (#:key tests? #:allow-other-keys)
+									      (when tests?
+										(invoke "pytest" "-k" "not download"))))))))
+		   (inputs
+		    `(("python-mutagen" ,python-mutagen)
+		      ("python-pycryptodomex" ,python-pycryptodomex)
+		      ("python-websockets" ,python-websockets)
+		      ,@(package-inputs youtube-dl)))
+		   (native-inputs
+		    `(("python-pytest" ,python-pytest)
+		      ,@(package-native-inputs youtube-dl)))
+		   (description
+		    "yt-dlp is a small command-line program to download videos from
+YouTube.com and many more sites.  It is a fork of youtube-dl with a
+focus on adding new features while keeping up-to-date with the
+original project.")
+		   (properties '((release-monitoring-url . "https://pypi.org/project/yt-dlp/")))
+		   (home-page "https://github.com/yt-dlp/yt-dlp")))
+
